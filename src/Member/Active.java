1078ee51c96d077d03b798783ddfd2138ecb577f
CilleElisabeth <cime1000@stud.ek.dk>
2026-05-07 11:24:38 +0200

    Tilføjet attributter, kontruktør, membersFee metode, getters

--- src/Member/Active.java
+++ src/Member/Active.java
@@ -1,4 +1,26 @@
 package Member;
 
-public class Active {
+public class Active extends Member {
+    private GameTypes gameType;
+    private Exerciser exercise;
+
+    public Active(String name, int memberID, char gender, int birthYear, int birthMonth, int birthDayOfMonth, GameTypes gameType, Exerciser exercise) {
+        super(name, memberID, gender, birthYear, birthMonth, birthDayOfMonth);
+        this.gameType = gameType;
+        this.exercise = exercise;
+    }
+    @Override public double membersFee() {
+        return  0.0;
+    }
+
+    public GameTypes getGameType() {
+        return gameType;
+    }
+
+    public Exerciser getExercise() {
+        return exercise;
+    }
+
+
+
 }
