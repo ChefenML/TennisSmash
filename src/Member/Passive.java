3c729ca191a245e73cf805a8d4c5e268548becf4
CilleElisabeth <cime1000@stud.ek.dk>
2026-05-07 12:27:52 +0200

    Tilføjet konstruktør og membersFee() i Passive.java

--- src/Member/Passive.java
+++ src/Member/Passive.java
@@ -1,4 +1,13 @@
 package Member;
 
-public class Passive {
+public class Passive extends Member {
+
+    public Passive(String name, int memberID, char gender, int birthYear, int birthMonth, int birthDayOfMonth) {
+        super(name, memberID, gender, birthYear, birthMonth, birthDayOfMonth); //Passive har kun felterne med fra Member
+    }
+
+    @Override public double membersFee() {
+        return 250;
+    }
+
 }
