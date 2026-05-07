b66be44a10499bc9ffad8e8005fbd086b19fbe33
idju1000 <idju1000@stud.ek.dk>
2026-05-07 11:37:07 +0200

    Junior,Senior,Veteran

--- src/Member/Senior.java
+++ src/Member/Senior.java
@@ -1,4 +1,11 @@
 package Member;
 
-public class Senior {
+public class Senior extends Active {
+        public Senior(String name, int MemberID, char gender,int birthYear,int birthMonth,int birthDay){
+            super(name,MemberID,gender,birthYear,birthMonth,birthDay);
+        }
+        @Override
+        public double membersFee(){
+           1500.0;
+        }
 }
--- src/Member/Veteran.java
+++ src/Member/Veteran.java
@@ -1,4 +1,13 @@
 package Member;
 
-public class Veteran {
-}
+public class Veteran extends Active {
+        public Veteran(String name, int MemberID, char gender,int birthYear,int birthMonth,int birthDay){
+            super(name,MemberID,gender,birthYear,birthMonth,birthDay);
+
+        }
+        @Override
+        public double membersFee(){
+           return 1125.0;
+        }
+    }
+
