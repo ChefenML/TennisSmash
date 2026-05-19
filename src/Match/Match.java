e63707677efe7f0b682c8d9e107acad4c8fdf6f6
ChefenML <milo1000@stud.ek.dk>
2026-05-19 11:26:20 +0200

    match first draft

---
+++ src/Match/Match.java
@@ -0,0 +1,43 @@
+package Match;
+
+import Member.*;
+
+import java.time.LocalDate;
+
+public class Match {
+    Member member1;
+    Member member2;
+    Member member3;
+    Member member4;
+    int result;
+    GameTypes gameTypes;
+    LocalDate date;
+    //Single Match data
+    public Match(Member member1, Member member2, int result, GameTypes gameTypes, LocalDate date){
+        this.member1 = member1;
+        this.member2 = member2;
+        this.result = result;
+        this.gameTypes = gameTypes;
+        this.date = date;
+
+    }
+    //Overloaded constructor for doubles & Mixed
+    public Match(Member member1, Member member2,Member member3, Member member4, int result, GameTypes gameTypes, LocalDate date){
+        this.member1 = member1;
+        this.member2 = member2;
+        this.member3 = member3;
+        this.member4 = member4;
+        this.result = result;
+        this.gameTypes = gameTypes;
+        this.date = date;
+
+    }
+
+    public int getResult(){
+        return result;
+    }
+
+    public int getWinSize(){
+        return Math.abs(result);
+    }
+}
