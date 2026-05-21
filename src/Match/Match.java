32a4274066821eaa2be0f370d82337e080ca231f
Heescheo <Heescheo@gmail.com>
2026-05-21 11:10:58 +0200

    Lavet public getters til int member1,2,3,4 i Match klassen og implementeret dem i public List<Match> getMatchesForMember(int memberId) metoden i MatchFactory klassen.

--- src/Match/Match.java
+++ src/Match/Match.java
@@ -7,10 +7,10 @@ import java.time.LocalDate;
 import static Member.GameTypes.SINGLE;
 
 public class Match {
-    int member1, member2, member3, member4;
-    int result;
-    GameTypes gameTypes;
-    LocalDate date;
+    private int member1, member2, member3, member4;
+    private int result;
+    private GameTypes gameTypes;
+    private LocalDate date;
     //Single Match data
     public Match(int member1, int member2, int result, LocalDate date){
         this.member1 = member1;
@@ -32,7 +32,23 @@ public class Match {
 
     }
 
-//    public int getMemberId(){
+    public int getMember1() {
+        return member1;
+    }
+
+    public int getMember2() {
+        return member2;
+    }
+
+    public int getMember3() {
+        return member3;
+    }
+
+    public int getMember4() {
+        return member4;
+    }
+
+    //    public int getMemberId(){
 //        return member.get
 //    }
 
