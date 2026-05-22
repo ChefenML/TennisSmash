07afdcc434c06192608ad251b5504e4c6e3401dd
ChefenML <milo1000.stud.ek.dk>
2026-05-22 11:29:32 +0200

    Check files on startup

--- src/util/CheckFiles.java
+++ src/util/CheckFiles.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class CheckFiles {
     private File match = new File("src/Data/match.csv");
-    private File members = new File("src/Data/member.csv");
+    private File members = new File("src/Data/members.csv");
     private File payments = new File("src/Data/payments.csv");
     private List<File> fileList = new ArrayList<>(List.of(match, members, payments));
 
@@ -15,11 +15,11 @@ public class CheckFiles {
     public void checkForFile() {
         for (File l : fileList) {
             if (l.exists()) {
-                System.out.println("Fil findes: " + l.getName());
+                System.out.println("Fil findes: " + l.getName() + " ✔");
             } else {
                 try {
                     if(l.createNewFile()) {
-                        System.out.println("Fil oprettet: " + l.getName());
+                        System.out.println("Fil mangler -> oprettet: " + l.getName() + " ✔");
                     }
                 } catch (IOException e) {
                     throw new RuntimeException(e);
