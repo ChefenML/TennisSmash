76ebd1516661a9bc7d12d626304345f32aad18a2
ChefenML <milo1000.stud.ek.dk>
2026-05-26 14:30:05 +0200

    Remove Tournament+Training handler

--- src/FileHandler/TrainingHandler.java
+++
@@ -1,7 +0,0 @@
-package FileHandler;
-
-import Payment.Payment;
-
-//public class TrainingHandler implements FileHandling<Training, TrainingLoader>{
-//    private static final String TRAINING_FILE = "src/Data/training.csv";
-//}
